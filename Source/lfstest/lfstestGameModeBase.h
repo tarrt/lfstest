674098f91fff2e8498ba1e0e4d5b7eed0f6e1507
tarrt <89452184+tarrt@users.noreply.github.com>
2021-08-24 19:17:58 +0300

    Initial commit

---
+++ Source/lfstest/lfstestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "lfstestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LFSTEST_API AlfstestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
